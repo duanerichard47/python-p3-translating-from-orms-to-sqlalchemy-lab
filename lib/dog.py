57d94d68d8ef152f23f877536a9f3561c354e72f
Duane <duanerichard47@hotmail.com>
2023-03-13 20:51:04 -0400

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -21,17 +21,19 @@ def get_all(session):
     
 
 def find_by_name(session, name):
-    return session.query(Dog).filter(Dog.name == name)
+    return session.query(Dog).filter(Dog.name == name).first()
     
 
 def find_by_id(session, id):
-    return session.query(Dog).filter(Dog.name.id == id)
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    return session.query(Dog).filter(Dog.name == name).filter(Dog.breed == breed)
-
+    # return session.query(Dog).filter(Dog.name == name).filter(Dog.breed == breed).first()
+    return session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
 def update_breed(session, dog, breed):
-    session.query(Dog).update(dog.breed == breed)
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
 
 
 
